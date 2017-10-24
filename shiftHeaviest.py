7a5a11a689158296c8fb47c1473b1b0c8ea50907
Vipin Kumar <31645405+vipinkjonwal@users.noreply.github.com>
2017-10-24 12:47:14 +0530

    Update shiftHeaviest.py

--- shiftHeaviest.py
+++ shiftHeaviest.py
@@ -1,13 +1,13 @@
 def shiftHeaviest(givenList,index,tempIndex=0):
 	'''
-	Objective			: To shift the heaviest element from index 0 to index 'index-1' at the index 'index'.
+	Objective		: To shift the heaviest element from index 0 to index 'index-1' at the index 'index'.
 	Input Parameters	: 
-			givenList	: List entered by the user.
-			index       : Index to which heaviest element is to be shifted.
-			tempIndex	: DEFAULT - 0 ; For iterations.
+		givenList	: List entered by the user.
+		    index       : Index to which heaviest element is to be shifted.
+		tempIndex	: DEFAULT - 0 ; For iterations.
 	Return Value		: None.
 	'''
-	#Approach			: To swap the number to right if it is larger than the other number 
+	#Approach		: To swap the number to right if it is larger than the other number 
 	#					  and recursively doing till it reaches to desired index. 
 
 	if tempIndex!=index:
@@ -25,11 +25,11 @@ def shiftHeaviest(givenList,index,tempIndex=0):
 
 def main():
 	'''
-	Objective			: To shift the heaviest element from index 0 to index 'index-1' at the index 'index'.
+	Objective		: To shift the heaviest element from index 0 to index 'index-1' at the index 'index'.
 	Input Parameters	: None.
 	Return Value		: None.
 	'''
-	#Approach			: Invoke the shiftHeaviest function.
+	#Approach		: Invoke the shiftHeaviest function.
 
 	givenList=[int(x) for x in input('Enter the elements of the list: ').split()]
 	print('You have entered: ',givenList,sep="")
