154827591f551d12ae20ae3dc4773ba36f94a5cb
Vipin Kumar <vipin.mca17.du@gmail.com>
2017-11-25 21:56:18 +0530

    Intersection of Two Lists
    Documentation Update

--- intersection.py
+++ intersection.py
@@ -2,6 +2,16 @@ list1 = [6,1,2,7,2,5,3,8,4]
 list2 = [1,3,5,7,9,11]
 
 def intersection(list1,list2,finalList=[]):
+    '''
+    Objective           : To find the intersection of two lists.
+    Input Parameters    :
+            :param list1: Given List 1
+            :param list2: Given List 2
+        :param finalList: Default - Null List, will contain intersection of given lists
+    Return Value        : finalList, the intersection of two lists.
+    '''
+    #Approach           : Iterative approach is used.
+
     lenList1 = len(list1)
     lenList2 = len(list2)
 
