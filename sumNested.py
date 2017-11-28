be00467813beaa7d717caf4e8061dfed57bde541
Vipin Kumar <vipin.mca17.du@gmail.com>
2017-11-28 22:35:21 +0530

    Sum of Elements of Nested List
    Nesting Upto Level One

--- sumNested.py
+++ sumNested.py
@@ -1,13 +1,25 @@
+def sumList(givenList,totalSum = 0,i = 0):
+    if len(givenList) == i:
+        return totalSum
+    else:
+        totalSum += givenList[i]
+        i += 1
+        return sumList(givenList,totalSum,i)
+
 def sumNested(givenList, totalSum = 0,i = 0):
     if len(givenList) == i:
         return totalSum
     elif type(givenList[i]) == int:
         totalSum += givenList[i]
         i += 1
+        return sumNested(givenList,totalSum,i)
     elif type(givenList[i]) == list:
-       return sumNested(givenList[i],totalSum,i)
+        tempSum = sumList(givenList[i])
+        totalSum += tempSum
+        return sumNested(givenList,totalSum,i+1)
 
 
-givenList = [1,2,3,[4,5],6,7,8,9,10]
+givenList = [1,[5],6,[1,2,4],5]
 totalSum = sumNested(givenList)
-print(totalSum)
+print('Given List',givenList)
+print('Sum of Elements of Nested List is',totalSum)
