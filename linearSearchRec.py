ae2f6b409126c2e58f03366e2123cfa6d090ef4a
Vipin Kumar <vipin.mca17.du@gmail.com>
2017-11-29 11:37:27 +0530

    Linear Search (Recursion)
    Index of Element Found

--- linearSearchRec.py
+++ linearSearchRec.py
@@ -1,17 +1,19 @@
-def linearSearch(givenList,element):
+def linearSearch(givenList,element,position=0):
     if len(givenList) == 0:
-        return False
+        return -1
     else:
         if givenList[0] == element:
-            return True
+            return position
         else:
-            return linearSearch(givenList[1:],element)
+            position += 1
+            return linearSearch(givenList[1:],element,position)
 
 givenList = [11,27,7,12,4,19,10,34]
 element = int(input('Enter the element: '))
-boolValue = linearSearch(givenList,element)
+positionElement = linearSearch(givenList,element)
 
-if boolValue == True:
-    print(element,'is present in the list.')
+print('Given List: ',givenList)
+if positionElement == -1:
+    print(element,'is NOT present in the list.')
 else:
-    print(element, 'is NOT present in the list.')
+    print(element,'is present in the list at location',positionElement+1)
