1e74a009cf01028c9897c233c836dde96bf7b59a
Vipin Kumar <vipin.mca17.du@gmail.com>
2017-12-05 21:18:52 +0530

    Fibonacci Numbers
    Documentation

--- fibonacci.py
+++ fibonacci.py
@@ -1,4 +1,10 @@
 def fibonacci(number):
+    '''
+    Objective                : Computes the Fibonacci Number at position 'number'
+    Input Variables          :
+                :param number: Given number at which position Fibonacci Number is expected.
+    Return Value             : Fibonacci Number at position 'number'
+    '''
     if number == 0 or number == 1:
         return 1
     else:
