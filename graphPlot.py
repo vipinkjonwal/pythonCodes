40151d93b796daa27a0ccda2cbcc5b3609d91264
Vipin Kumar <vipin.mca17.du@gmail.com>
2017-12-12 22:06:52 +0530

    Basic Histogram Plotting

---
+++ graphPlot.py
@@ -0,0 +1,6 @@
+import matplotlib.pyplot as plt
+
+x = [21, 22, 23, 4, 5, 6, 77, 8, 9, 10, 31, 32, 33, 34, 35, 36, 37, 18, 49, 50, 100]
+num_bins = 8
+n, bins, patches = plt.hist(x, num_bins, facecolor='blue', alpha=0.5)
+plt.show()
