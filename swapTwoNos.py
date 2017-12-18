4b7f420d64669f07b193fd32fbcf52499b5c276c
Vipin Kumar <vipin.mca17.du@gmail.com>
2017-12-18 19:13:21 +0530

    Swap Two Numbers

---
+++ swapTwoNos.py
@@ -0,0 +1,38 @@
+def swap1(a,b):
+    print('Method 1')
+    print('Before Swapping : a : ',a,' & b : ',b)
+    temp = a
+    a= b
+    b = temp
+    print('After Swapping : a : ', a, ' & b : ', b)
+
+def swap2(a, b):
+    print('Method 2')
+    print('Before Swapping : a : ', a, ' & b : ', b)
+    a = a * b
+    b = a / b
+    a = a / b
+    print('After Swapping : a : ', a, ' & b : ', b)
+
+def swap3(a, b):
+    print('Method 3')
+    print('Before Swapping : a : ', a, ' & b : ', b)
+    a = a + b
+    b = a - b
+    a = a - b
+    print('After Swapping : a : ', a, ' & b : ', b)
+
+def swap4(a, b):
+    print('Method 4')
+    print('Before Swapping : a : ', a, ' & b : ', b)
+    a = a ^ b
+    b = a ^ b
+    a = a ^ b
+    print('After Swapping : a : ', a, ' & b : ', b)
+
+swap1(1,2)
+swap2(1,2)
+swap3(1,2)
+swap4(1,2)
+
+
