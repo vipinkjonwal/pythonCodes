f79fa569ed0919b6d2e1f1282983189e9b758366
Vipin Kumar <vipin.mca17.du@gmail.com>
2017-12-20 13:06:38 +0530

    Swap Two Numbers

--- swapTwoNos.py
+++ swapTwoNos.py
@@ -1,4 +1,7 @@
 import sys
+import timeit
+
+
 def swap1(a,b):
     print('Method 1')
     print('Before Swapping : a : ',a,' & b : ',b)
@@ -31,7 +34,42 @@ def swap4(a, b):
     a = a ^ b
     print('After Swapping : a : ', a, ' & b : ', b)
 
-print('Version',sys.version)
+TEST_CODE = '''
+def swap1(a,b):
+    print('Method 1')
+    print('Before Swapping : a : ',a,' & b : ',b)
+    temp = a
+    a= b
+    b = temp
+    print('After Swapping : a : ', a, ' & b : ', b)
+
+def swap2(a, b):
+    print('Method 2')
+    print('Before Swapping : a : ', a, ' & b : ', b)
+    a = a * b
+    b = a / b
+    a = a / b
+    print('After Swapping : a : ', a, ' & b : ', b)
+
+def swap3(a, b):
+    print('Method 3')
+    print('Before Swapping : a : ', a, ' & b : ', b)
+    a = a + b
+    b = a - b
+    a = a - b
+    print('After Swapping : a : ', a, ' & b : ', b)
+
+def swap4(a, b):
+    print('Method 4')
+    print('Before Swapping : a : ', a, ' & b : ', b)
+    a = a ^ b
+    b = a ^ b
+    a = a ^ b
+    print('After Swapping : a : ', a, ' & b : ', b)
+'''
+
+print(timeit.timeit(stmt=TEST_CODE,number=100000))
+print('Ver',sys.version)
 swap1(1,2)
 swap2(1,2)
 swap3(1,2)
