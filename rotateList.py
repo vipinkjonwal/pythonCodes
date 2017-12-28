6dc4d5daa4399de768b26cf03f74c16e66c93867
Vipin Kumar <vipin.mca17.du@gmail.com>
2017-12-28 21:12:27 +0530

    Rotate List (Two Methods)

--- rotateList.py
+++ rotateList.py
@@ -1,3 +1,4 @@
+import timeit
 def rotateList(givenList):
     temp = givenList[0]
     for i in range(len(givenList)-1):
@@ -12,6 +13,10 @@ def listRotate(givenList):
 
 givenList = [1,2,3,4,5]
 print(rotateList(givenList))
+t1 = timeit.Timer(lambda: rotateList(givenList))
+print('Method 1:',t1.timeit())
 
 givenList = [1,2,3,4,5]
 print(listRotate(givenList))
+t2 = timeit.Timer(lambda: listRotate(givenList))
+print('Method 2:',t2.timeit())
