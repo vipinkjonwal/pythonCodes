7545d602f79315354a9f8bbe7fe1aa4b28ca3aab
Vipin Kumar <vipin.mca17.du@gmail.com>
2017-12-28 21:08:17 +0530

    Average of Three Numbers* * (More Optimized Way Needed)

--- averageOfThree.py
+++ averageOfThree.py
@@ -2,7 +2,7 @@ def averageTwo(num1, num2):
     return (num1 + num2)/2
 
 def averageThree(num1,num2,num3):
-    return averageTwo(averageTwo(num1,num2),averageTwo(num2,num3))
+    return (num1 + num2 + num3)/3
 
 print(averageThree(1,2,3))
 print(averageThree(11,2,13))
