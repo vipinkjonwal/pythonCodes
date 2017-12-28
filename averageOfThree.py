ea21a8e412d4f1c8b8da51c5fab019a4b4630da3
Vipin Kumar <vipin.mca17.du@gmail.com>
2017-12-28 21:06:34 +0530

    Rotate List

---
+++ averageOfThree.py
@@ -0,0 +1,13 @@
+def averageTwo(num1, num2):
+    return (num1 + num2)/2
+
+def averageThree(num1,num2,num3):
+    return averageTwo(averageTwo(num1,num2),averageTwo(num2,num3))
+
+print(averageThree(1,2,3))
+print(averageThree(11,2,13))
+print(averageThree(1,12,3))
+
+
+
+
--- rotateList.py
+++ rotateList.py
@@ -5,5 +5,13 @@ def rotateList(givenList):
     givenList[len(givenList)-1] = temp
     return givenList
 
+def listRotate(givenList):
+    for i in range(len(givenList)-1):
+        givenList[i],givenList[i+1] = givenList[i+1],givenList[i]
+    return givenList
+
 givenList = [1,2,3,4,5]
 print(rotateList(givenList))
+
+givenList = [1,2,3,4,5]
+print(listRotate(givenList))
