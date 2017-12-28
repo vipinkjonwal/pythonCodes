dbc3301eb9a8fedef88bb062a30890dcccc154f1
Vipin Kumar <vipin.mca17.du@gmail.com>
2017-12-28 21:25:00 +0530

    Swap Two Numbers (4 Methods)

--- swapTwoNos.py
+++ swapTwoNos.py
@@ -1,78 +1,42 @@
-import sys
 import timeit
 
-
 def swap1(a,b):
-    print('Method 1')
-    print('Before Swapping : a : ',a,' & b : ',b)
     temp = a
     a= b
     b = temp
-    print('After Swapping : a : ', a, ' & b : ', b)
 
 def swap2(a, b):
-    print('Method 2')
-    print('Before Swapping : a : ', a, ' & b : ', b)
     a = a * b
     b = a / b
     a = a / b
-    print('After Swapping : a : ', a, ' & b : ', b)
 
 def swap3(a, b):
-    print('Method 3')
-    print('Before Swapping : a : ', a, ' & b : ', b)
     a = a + b
     b = a - b
     a = a - b
-    print('After Swapping : a : ', a, ' & b : ', b)
 
 def swap4(a, b):
-    print('Method 4')
-    print('Before Swapping : a : ', a, ' & b : ', b)
     a = a ^ b
     b = a ^ b
     a = a ^ b
-    print('After Swapping : a : ', a, ' & b : ', b)
 
-TEST_CODE = '''
-def swap1(a,b):
-    print('Method 1')
-    print('Before Swapping : a : ',a,' & b : ',b)
-    temp = a
-    a= b
-    b = temp
-    print('After Swapping : a : ', a, ' & b : ', b)
+t1 = timeit.Timer(lambda: swap1(1,2))
+print('Method 1 (Using Temp):',t1.timeit())
 
-def swap2(a, b):
-    print('Method 2')
-    print('Before Swapping : a : ', a, ' & b : ', b)
-    a = a * b
-    b = a / b
-    a = a / b
-    print('After Swapping : a : ', a, ' & b : ', b)
+t2 = timeit.Timer(lambda: swap2(1,2))
+print('Method 2 (Using * and /):',t2.timeit())
 
-def swap3(a, b):
-    print('Method 3')
-    print('Before Swapping : a : ', a, ' & b : ', b)
-    a = a + b
-    b = a - b
-    a = a - b
-    print('After Swapping : a : ', a, ' & b : ', b)
+t3 = timeit.Timer(lambda: swap3(1,2))
+print('Method 3 (Using + and -):',t3.timeit())
 
-def swap4(a, b):
-    print('Method 4')
-    print('Before Swapping : a : ', a, ' & b : ', b)
-    a = a ^ b
-    b = a ^ b
-    a = a ^ b
-    print('After Swapping : a : ', a, ' & b : ', b)
+t4 = timeit.Timer(lambda: swap4(1,2))
+print('Method 4 (Using XOR):',t4.timeit())
+
+
 '''
-
-print(timeit.timeit(stmt=TEST_CODE,number=100000))
-print('Ver',sys.version)
 swap1(1,2)
 swap2(1,2)
 swap3(1,2)
 swap4(1,2)
-
+'''
 
