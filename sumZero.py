60a52ed0f4fcf06c88308fa58eb1748aa3fcc9ca
Vipin Kumar <vipin.mca17.du@gmail.com>
2018-01-07 13:27:58 +0530

    Find pairs whose sum is Zero

---
+++ sumZero.py
@@ -0,0 +1,14 @@
+def findPairs(givenList):
+    pairsList = []
+
+    for i in givenList:
+        for j in givenList:
+            if i+j == 0:
+                pairsList.append((i,j))
+                givenList.remove(j)
+
+    return pairsList
+
+givenList = [-3,1,4,6,-2,3,2,-1,7,-4]
+pairsList = findPairs(givenList)
+print(pairsList)
