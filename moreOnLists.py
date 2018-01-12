fe45e17caada914c9923775bfd2661325d18b5a5
Vipin Kumar <vipin.mca17.du@gmail.com>
2018-01-12 20:37:45 +0530

    Stack using List

---
+++ moreOnLists.py
@@ -0,0 +1,21 @@
+from collections import deque
+def push(stack,item):
+    stack.append(item)
+    return stack
+
+def myPop(stack):
+    item = stack.pop()
+    return item
+
+stack = []
+print('Pushing onto Stack')
+print(push(stack,2))
+print(push(stack,3))
+print(push(stack,5))
+print(push(stack,7))
+
+print('\nPop from Stack')
+myPop(stack)
+print(stack)
+myPop(stack)
+print(stack)
