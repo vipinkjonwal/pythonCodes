2dd2309bb1ae448c987b128afbff0d1cde60ffa1
Vipin Kumar <vipin.mca17.du@gmail.com>
2018-01-14 20:53:44 +0530

    Remove Duplicates from List

---
+++ removeDuplicates.py
@@ -0,0 +1,13 @@
+def removeDuplicate(givenList):
+    '''
+    Objective               : To remove duplicates from the list.
+    Input Parameters
+                   givenList: Given list with duplicate values.
+    Return Value            : Given list with duplicates removed.
+    '''
+    # Approach              : Set properties is used to remove duplicates.
+
+    return list(set(givenList))
+
+givenList = [1,2,3,4,5,4,3,2,3,4,5,6]
+print(removeDuplicate(givenList))
