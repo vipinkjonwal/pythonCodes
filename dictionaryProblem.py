dc2f23a3909fc903b34b1b1af77af512a56842b8
Vipin Kumar <vipin.mca17.du@gmail.com>
2018-01-26 23:15:02 +0530

    Dictionary Problem

--- dictionaryProblem.py
+++ dictionaryProblem.py
@@ -6,6 +6,14 @@ semesterLectures = {'January':[5, 6, 5, 4, 5],
                     }
 
 def totalAttendance(semLectures):
+    '''
+    Objective               : To calculate the total attendances given as list in dictionary values as per weel.
+    Input Parameters        :
+                 semLectures: Given dictionary with values in form of dictionary.
+    Return Value            : Total attendance.
+    '''
+    # Approach              : for loop is used to calculate the sum of values of dictionary.
+
     finalSum = 0
     for keys in semesterLectures.keys():
         finalSum += sum(semesterLectures[keys])
