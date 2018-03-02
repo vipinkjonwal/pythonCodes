f0108f426d7c985e2dfc7478adc54fde20787466
Vipin Kumar <vipin.mca17.du@gmail.com>
2018-03-02 21:51:19 +0530

    xml Update

---
+++ commonElements.py
@@ -0,0 +1,12 @@
+def commonElements(list1,list2):
+    set1 = set(list1)
+    set2 = set(list2)
+
+    commonElementsSet = set1 & set2
+
+    return list(commonElementsSet)
+
+lis1 = [1,2,3,4,5]
+list2 = [3,4,5,6,7,8]
+
+print(commonElements(lis1,list2))
