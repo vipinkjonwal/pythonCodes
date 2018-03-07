91dce10eeb510e3ca3e9474fb7ce1cbcbefa5f9c
Vipin Kumar <vipin.mca17.du@gmail.com>
2018-03-07 21:20:14 +0530

    Cricbuzz API Learn

--- cricbuzz.py
+++ cricbuzz.py
@@ -5,8 +5,18 @@ from pprint import  pprint
 f = open('output.txt','w')
 c = Cricbuzz()
 matches = c.matches()
+scorecard = c.scorecard(18775)
+
+# pprint(scorecard)
+
 
 for i in range(len(matches)):
-    pprint(matches)
+    print('Match: ',end='')
+    print(matches[i]['mchdesc'])
+    print('Result: ',end='')
+    print(matches[i]['status'],end='')
+    print('\n')
 
-# print(json.dumps(matches,indent=4),file=f)
+'''
+print(json.dumps(matches,indent=4),file=f)
+'''
